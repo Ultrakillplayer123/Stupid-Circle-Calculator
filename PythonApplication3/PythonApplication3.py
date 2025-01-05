79a7338bd284c7c2ea796763a9a7f7a6e00c9bfc
Kristian <Kristian@K>
2025-01-05 14:23:08 -0600

    Add project files.

---
+++ PythonApplication3/PythonApplication3.py
@@ -0,0 +1,11 @@
+#CircleCalculator
+#This program calculates the area and circumference of a circle
+#The user is prompted to enter the radius of the circle
+from math import pi
+
+r=4
+n=pi
+print(n*r**2)
+
+C=2*n*r
+print(C)
